c01f3bd35116a433e889c576a859e2ffb526fd4e
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-03 17:20:54 +0200

    userservice class added role selection in create method, readByEmail method and isAdmin

--- src/main/java/org/university/bookQuest/service/UserService.java
+++ src/main/java/org/university/bookQuest/service/UserService.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.university.bookQuest.entity.Role;
 import org.university.bookQuest.entity.User;
 import org.university.bookQuest.repository.UserRepository;
 
@@ -15,9 +16,11 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleService roleService;
 
-    public User create(User user) {
+    public User create(User user, Role role) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(role);
         userRepository.saveAndFlush(user);
         log.info("registered - {}", user);
         return user;
@@ -30,8 +33,19 @@ public class UserService {
         return user;
     }
 
+    public User readByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User not found!"));
+        log.info("read user with email {}", user.getEmail());
+        return user;
+    }
+
     public void delete(long id) {
         userRepository.delete(readById(id));
         log.info("user with id {} successfully deleted", id);
+    }
+
+    public boolean isAdmin(long id) {
+        return roleService.getByUserId(id).getName().equals("ADMIN");
     }
 }
