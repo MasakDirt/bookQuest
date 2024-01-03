e0f33679367fb4624c0ccd567d5e60237d8471f2
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-03 23:34:50 +0200

    remove unused imports

--- src/main/java/org/university/bookQuest/repository/RoleRepository.java
+++ src/main/java/org/university/bookQuest/repository/RoleRepository.java
@@ -1,7 +1,6 @@
 package org.university.bookQuest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.university.bookQuest.entity.Role;
 
